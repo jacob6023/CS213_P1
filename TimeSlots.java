3dc0bf0806df1b4aa9659c60510b8e01a44fbe93
PeanutBread1 <102591143+PeanutBread1@users.noreply.github.com>
2024-09-30 15:02:53 -0400

    Update TimeSlots.java

--- TimeSlots.java
+++ TimeSlots.java
@@ -1,21 +1,18 @@
-public enum Timeslot {
-    SLOT1(9, 0),
-    SLOT2(10, 45),
-    SLOT3(11, 15),
-    SLOT4(13, 30),
-    SLOT5(15, 0),
-    SLOT6(16, 15);
+package org.example;
 
+public enum Timeslot {
+    SLOT1 (9, 0),
+    SLOT2 (10, 45),
+    SLOT3 (11, 15),
+    SLOT4 (13, 30),
+    SLOT5 (15, 0),
+    SLOT6 (16, 15);
     private final int hour;
     private final int minute;
 
-    Timeslot(int hour, int minute) {
+    Timeslot(int hour, int minute){
         this.hour = hour;
         this.minute = minute;
     }
 
-    @Override
-    public String toString() {
-        return String.format("%02d:%02d", hour, minute);
-    }
 }
