964749e392c622086060a536129f49020d9ce320
PeanutBread1 <102591143+PeanutBread1@users.noreply.github.com>
2024-09-30 15:01:25 -0400

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class Date implements Comparable<Date> {
     private int year;
     private int month;
@@ -9,49 +11,46 @@ public class Date implements Comparable<Date> {
         this.day = day;
     }
 
-    public boolean isValid() {
-        if (year < 1 || month < 1 || month > 12 || day < 1) {
-            return false;
-        }
-        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if (isLeapYear(year)) {
-            daysInMonth[2] = 29;
-        }
-        return day <= daysInMonth[month];
+    public int getYear() {
+        return year;
     }
 
-    private boolean isLeapYear(int year) {
-        if (year % 4 != 0) {
-            return false;
-        } else if (year % 100 != 0) {
-            return true;
-        } else {
-            return year % 400 == 0;
-        }
+    public int getMonth() {
+        return month;
     }
 
-    @Override
-    public int compareTo(Date other) {
-        if (this.year != other.year) {
-            return Integer.compare(this.year, other.year);
-        } else if (this.month != other.month) {
-            return Integer.compare(this.month, other.month);
-        } else {
-            return Integer.compare(this.day, other.day);
-        }
+    public int getDay() {
+        return day;
     }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public boolean isValid(Date date){
+        return true;
+    } //check if the date is a valid calendar date
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Date) {
-            Date other = (Date) obj;
-            return this.year == other.year && this.month == other.month && this.day == other.day;
+    public int compareTo(Date o) {
+        if(this.year != o.year){
+            return this.year - o.year;
         }
-        return false;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%02d/%02d/%04d", month, day, year);
+        if(this.month != o.month){
+            return this.month - o.month;
+        }
+        if(this.day != o.day){
+            return this.day - o.day;
+        }
+        return 0;
     }
 }
+
