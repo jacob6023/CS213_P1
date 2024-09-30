edf57d743c92f3cbdea558b306600eaf05e6286c
PeanutBread1 <102591143+PeanutBread1@users.noreply.github.com>
2024-09-30 15:01:43 -0400

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,36 +1,12 @@
-public class Profile implements Comparable<Profile> {
+package org.example;
+
+public class Profile implements Comparable<Profile>{
     private String fname;
     private String lname;
     private Date dob;
 
-    public Profile(String fname, String lname, Date dob) {
-        this.fname = fname;
-        this.lname = lname;
-        this.dob = dob;
-    }
-
     @Override
-    public int compareTo(Profile other) {
-        int lastCmp = this.lname.compareToIgnoreCase(other.lname);
-        if (lastCmp != 0) return lastCmp;
-        int firstCmp = this.fname.compareToIgnoreCase(other.fname);
-        if (firstCmp != 0) return firstCmp;
-        return this.dob.compareTo(other.dob);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Profile) {
-            Profile other = (Profile) obj;
-            return this.fname.equalsIgnoreCase(other.fname) &&
-                    this.lname.equalsIgnoreCase(other.lname) &&
-                    this.dob.equals(other.dob);
-        }
-        return false;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s %s, DOB: %s", fname, lname, dob);
+    public int compareTo(Profile o) {
+        return 0;
     }
 }
